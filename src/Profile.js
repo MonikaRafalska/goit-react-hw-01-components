3008624a6b21dd6430a060d0cc90cf95f1859c73
Monika Rafalska <82111615+MonikaRafalska@users.noreply.github.com>
2022-02-05 22:16:16 +0100

    start

---
+++ src/Profile.js
@@ -0,0 +1,42 @@
+import styles from "./Profile.module.css";
+import PropTypes, { object } from "prop-types";
+
+const Profile = (user) => (
+  <div className={styles.profile}>
+    <div className={styles.description}>
+      <img
+        src={user.avatar}
+        alt={user.username + "avatar"}
+        className={styles.avatar}
+      />
+      <p className={styles.name}>{user.username}</p>
+      <p className={styles.tag}>@{user.tag}</p>
+      <p className={styles.location}>{user.location}</p>
+    </div>
+
+    <ul className={styles.stats}>
+      <li>
+        <span className={styles.label}>Followers</span>
+        <span className={styles.quantity}>{user.stats.followers}</span>
+      </li>
+      <li>
+        <span className={styles.label}>Views</span>
+        <span className={styles.quantity}>{user.stats.views}</span>
+      </li>
+      <li>
+        <span className={styles.label}>Likes</span>
+        <span className={styles.quantity}>{user.stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
+};
+
+export default Profile;
